e7e8af9f515aea754d70c9d6a40400675c8e8234
Anushree pati <patianushree02@gmail.com>
2024-12-19 08:07:54 +0000

    data types.js

---
+++ 00-basics/data type.js
@@ -0,0 +1,28 @@
+"use strict";// treat all JS code newer version
+
+// alert(4+4) // we are using nodejs, not browser
+
+console.log(4+4)
+
+console.log("Anu")
+
+let name = "Anu"
+let age = 23
+let isloggedIn = false/true
+
+
+// number => 2 to power 54
+// bigint
+// string // use doublecoutr""
+// boolean => true/false
+// null => standalone value
+// undefined => now value not assigned
+// symbol => uniquness
+
+
+// object => 
+
+console.log(typeof "Anu")
+console.log(typeof age)
+console.log(typeof null);
+console.log(typeof undefined);
