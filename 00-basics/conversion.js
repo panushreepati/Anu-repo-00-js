b6352d4cab4eb974d80a3a6eb851c70e81ed4208
Anushree pati <patianushree02@gmail.com>
2024-12-22 14:33:00 +0000

    conversion.js

---
+++ 00-basics/conversion.js
@@ -0,0 +1,39 @@
+let score = null
+
+console.log(typeof score );
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueNumber);
+console.log(typeof(score));
+
+
+// let score = false => 0
+// let score = true => 1
+// let score = undefined => NAN (NOT A NUMBER)
+// let score = null => NAN
+// let score = "35" => string
+// let score = 35 => number
+// let score = "35abc" => NAN
+// let score = 35abc => error
+
+let isLoggedIn = ?(put value)
+
+
+let booleanIsLoggedInn Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// let isLoggedIn = 1 => true
+// let isLoggedIn = 0 => false
+// let isLoggedIn = ""(empty string) => false
+// let isLoggedIn = "Anu"=> true
+
+let someNumber = 35;
+
+let stringNumber string(someNumber);
+// console.log(string Number);
+console.log(typeof string);
+
+// 35
+// string
