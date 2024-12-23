d752a52f10defc68651ef10ec89140c5058b0926
Anushree pati <patianushree02@gmail.com>
2024-12-23 03:38:35 +0000

    operations.js

---
+++ 00-basics/operations.js
@@ -0,0 +1,37 @@
+let value = 4
+let negValue = value
+//console.log(negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(3%3);
+
+let str1 = "hello"
+let str2 = "Anu"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 4); // when (string)"" in starting its convert all things in string
+// console.log(1 + "4"); // when (string)"" in last its convert all things automatically convert
+// console.log("1" + "4");
+// console.log(1 + 4);
+// console.log("1" + 4 + 4);
+// console.log(1 + "4" + 4);
+// console.log(1 + 4 + "4");
+// console.log(1 + "4" + "4");
+// console.log(1 + 4 + 4);
+// console.log("1" + "4" + "4");
+
+//console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 4 + 4
+
+let gameCounter = 200 
+++gameCounter;
+console.log(gameCounter);
