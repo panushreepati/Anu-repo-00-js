ef8d2c648cdc81953440694f2e36b51888088e7c
Anushree pati <patianushree02@gmail.com>
2024-12-24 08:11:36 +0000

    comparison of datatypes.js

---
+++ 00-basics/comparison of datatypes.js
@@ -0,0 +1,21 @@
+// console.log(3 > 1);
+// console.log(3 >= 2);
+// console.log(3 < 2);
+// console.log(2 == 1);
+// console.log(3 != 2);
+
+
+// console.log("2" > 1);
+// console.log("03" > 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+
+console.log("4" === 4);
