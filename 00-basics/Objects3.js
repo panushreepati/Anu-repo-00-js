ff63d8c1e0365aa0aed594374c767b1c7bba9068
Anushree Pati <panushree1234@gmail.com>
2025-01-21 11:53:07 +0530

    JSON(objects3.js)

---
+++ 00-basics/Objects3.js
@@ -0,0 +1,26 @@
+const course = {
+    courcename: "js in code",
+    price: "888",
+    courseInstructor: "Anu"
+}
+
+// course.courseInstructor
+
+
+//const {courseInstructor} = course
+const {courseInstructor: instructor} = course
+
+//console.log(courseInstructor);
+//console.log(instructor)
+
+// {
+//     name: "anushee"
+//     courcename: "js in code"
+//     pticr:"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
