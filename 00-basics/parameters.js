53575ff0f567b45824b21d649e372781f167e8eb
Anushree Pati <panushree1234@gmail.com>
2025-01-21 21:30:30 +0530

    parameters.js

---
+++ 00-basics/parameters.js
@@ -0,0 +1,42 @@
+
+function sayMyName(){
+console.log("A");
+console.log("N");
+console.log("U");
+console.log("S");
+console.log("H");
+console.log("R");
+console.log("E");
+console.log("E");
+}
+
+//sayMyName()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+//addTwoNumbers(3,5)
+ const result = addTwoNumbers(3,5)
+
+//console.log("result", result);
+
+
+ function addTwoNumbers(number1, number2){
+  // let result = number1 + number2
+   //console.log("Anushree");
+  // return result
+}  
+
+
+function loginUsermessage(username){
+    if (username === undefined){
+        console.log("please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+
+console.log(loginUsermessage())
+
