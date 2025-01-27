e762a435ca1ed63ac7d54686e50a15bffdf93908
Anushree Pati <panushree1234@gmail.com>
2025-01-27 20:59:07 +0530

    Hoisting.js

---
+++ 00-basics/Hoisting.js
@@ -0,0 +1,52 @@
+function one (){
+    const username = "anushree"
+
+    function two(){
+        const website = "code"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+
+}
+
+//one()
+
+if (true) {
+    const username = "anushree"
+    if (username === "anushree") {
+        const website = "chrome"
+       // console.log(username + website);
+
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+
+function addone(num){
+    return num + 1
+}
+
+addone(5)
+//console.log(addone(5));
+
+function addTwo(num){
+    return num + 2
+}
+
+addTwo(5)
+//console.log(addTwo(5));
+
+//addone(5)
+//console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
