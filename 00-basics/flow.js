547c5db7635ee20be15edf3f9c99af9ad54f1d84
Anushree Pati <panushree1234@gmail.com>
2025-02-02 19:15:03 +0530

    flow.js

---
+++ 00-basics/flow.js
@@ -0,0 +1,99 @@
+// if 
+// const isUserloggedIn = true
+
+// if ( 2 < "3" ){
+//      //console.log("executed");
+// }
+
+
+//  if ( 2 > "3" ){
+//      //console.log("executed");
+//  }
+
+
+//  if ( 2 <= "4" ){
+//     // console.log("executed");
+//  }
+
+//  if ( 2 >= "4" ){
+//     // console.log("executed");
+//  }  
+
+//  if ( 2 == "2" ){
+//     // console.log("executed");
+//  }
+
+//  if ( 2 != "3" ){
+//     // console.log("executed");
+//  }
+
+//  if ( 2 === "2" ){
+//    //  console.log("executed");
+//  }
+
+//  if ( 2 !== "3" ){
+//      console.log("executed");
+//  }
+  // <,>,<=,>=,==,!=,===,!==
+
+
+//   const isUserloggedIn = true
+//   const temperature = 42
+
+
+//   if (2 != 3 ) {
+//    // console.log("executed");
+//   }
+
+//   const isUserloggedIn = true
+//   const tempetature = 42
+
+//  if ( temperature < 50){
+//    console.log("less thsan 50");
+
+//  } else {
+//    console.log("temperature is greater than 50");
+//  }
+//   console.log("executed");
+
+// const score = 300 
+
+// if (score > 200) {
+//    const power = "dry"
+//    console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+
+
+const balance = 10000
+
+// if (balance > 5000) console.log("test"), console.log("test2"); // its a not propper way to write code
+
+//  if (balance > 500) {
+//     console.log("less than 500");
+
+// } else if (balance < 750) {
+//     console.log("less than 750");
+
+//  } else if (balance < 900) {
+//    console.log("less than 750");
+
+//  } else {
+//     console.log("less than 1200");
+//  }
+
+ const UserloggedIn = true
+ const debitcard = true
+ const loggedInfromGoogle = false
+ const loggedInEmail = true
+
+ if (UserloggedIn && debitcard && 2==3) {
+    console.log("Allow to buy course");
+}
+ 
+ if (loggedInfromGoogle || loggedInEmail ) { // (||) pipesign
+    console.log("user logged in")
+
+ }
