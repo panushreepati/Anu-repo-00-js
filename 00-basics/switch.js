598ab933c390d7025c9850b4066acc7232135777
Anushree Pati <panushree1234@gmail.com>
2025-02-02 20:05:35 +0530

    switch.js

---
+++ 00-basics/switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april"
+
+switch (month) {
+    case "january":
+        console.log("january");
+    break;
+
+    case "february":
+        console.log("february");
+    break;
+s
+    case "march":
+        console.log("march");
+    break;
+
+    case "april":
+        console.log("april");
+    break;
+
+    default:
+        console.log("default case match")
+        break;
+}
